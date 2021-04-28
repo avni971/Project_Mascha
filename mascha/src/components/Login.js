04228a476564c9ac040d4ac4b1e6868f12fa6c4a
yuval <avni971@gmail.com>
2021-04-28 17:14:52 +0300

    add project

---
+++ mascha/src/components/Login.js
@@ -0,0 +1,11 @@
+import{Link} from 'react-router-dom'
+const Login = () => {
+    return (
+        <div>
+           <h4>this is where our question form shall exist</h4> 
+           <Link to='/'>Go Back</Link>
+        </div>
+    )
+}
+
+export default Login
---
+++ mascha/src/components/Task.js
@@ -0,0 +1,22 @@
+import {FaTimes} from 'react-icons/fa'
+const Task = ({task,onDelete,onToggle}) => {
+    return (
+        <div className={`task ${task.reminder? 
+    'reminder': ''}`}       //if we have reminder on toggle add to class neame the reminder word
+     onDoubleClick={()=>
+        onToggle(task.id)}>
+            <h3>
+            {task.text}{' '}
+            <FaTimes        //close button
+            style={{color:'red',
+            cursor:'pointer'}} 
+            onClick={()=> onDelete(task.id)}    //if u press it close the task
+            />
+            </h3>
+            <p>{task.day}</p>
+
+        </div>
+    )
+}
+
+export default Task
